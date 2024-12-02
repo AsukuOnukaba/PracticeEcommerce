6f1fa11a7dc4180927c68ef8e47e10239577242a
Asuku <131924063+AsukuOnukaba@users.noreply.github.com>
2024-12-02 15:02:03 +0100

    Delete api directory

--- api/template/banner.php
+++
@@ -1,5 +0,0 @@
-<section id="banner" class="section-m1">
-    <h2><span>HOT Sales and Offers </span> are available now!!!</h2>
-    <a href="shop.php" class="bannerButton" >Explore more </a>
-
-</section>
